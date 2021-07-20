7987332b7e7bfb4ed75b6b3267fabef36d8264b3
Alec Thomas <aat@squareup.com>
2021-07-20 16:50:28 +1000

    Deserialise and print returned rows.
    Nothing is able to return rows yet because we can't get data into Prana,
    but it should theoretically work. Will whip up some tests in a followup
    PR.

--- cmd/prana/main.go
+++ cmd/prana/main.go
@@ -11,11 +11,13 @@ import (
 	"github.com/chzyer/readline"
 	"google.golang.org/grpc"
 
+	"github.com/squareup/pranadb/common"
 	"github.com/squareup/pranadb/protos/squareup/cash/pranadb/v1/service"
 )
 
 var cli struct {
 	Addr string `help:"Address of PranaDB server to connect to." default:"127.0.0.1:6584"`
+	VI   bool   `help:"Enable VI mode."`
 }
 
 func main() {
@@ -33,6 +35,7 @@ func main() {
 	rl, err := readline.NewEx(&readline.Config{
 		HistoryFile:            filepath.Join(home, ".prana.history"),
 		DisableAutoSaveHistory: true,
+		VimMode:                cli.VI,
 	})
 	kctx.FatalIfErrorf(err)
 	for {
@@ -43,6 +46,9 @@ func main() {
 			line, err := rl.Readline()
 			kctx.FatalIfErrorf(err)
 			line = strings.TrimSpace(line)
+			if line == "" {
+				continue
+			}
 			cmd = append(cmd, line)
 			if strings.HasSuffix(line, ";") {
 				break
@@ -57,8 +63,62 @@ func main() {
 		kctx.FatalIfErrorf(err)
 		switch result := resp.Result.(type) {
 		case *service.ExecuteSQLStatementResponse_Results:
+			names, types := toColumnTypes(result.Results)
+			rowsf := common.NewRowsFactory(types)
+			rows := rowsf.NewRows(int(result.Results.RowCount))
+			rows.Deserialize(result.Results.Rows)
+			if result.Results.RowCount != 0 {
+				fmt.Println(strings.Join(names, "|"))
+			}
+			rowstr := make([]string, len(rows.ColumnTypes()))
+			for ri := 0; ri < rows.RowCount(); ri++ {
+				row := rows.GetRow(ri)
+				for ci, ct := range rows.ColumnTypes() {
+					switch ct.Type {
+					case common.TypeVarchar:
+						rowstr[ci] = row.GetString(ci)
+					case common.TypeTinyInt, common.TypeBigInt, common.TypeInt:
+						rowstr[ci] = fmt.Sprintf("%v", row.GetInt64(ci))
+					case common.TypeDecimal:
+						dec := row.GetDecimal(ci)
+						rowstr[ci] = dec.String()
+					case common.TypeDouble:
+						rowstr[ci] = fmt.Sprintf("%g", row.GetFloat64(ci))
+					case common.TypeTimestamp:
+						rowstr[ci] = row.GetString(ci)
+					case common.TypeUnknown:
+						rowstr[ci] = "??"
+					}
+				}
+				fmt.Println(strings.Join(rowstr, "|"))
+			}
+			if result.Results.RowCount == 1 {
+				fmt.Printf("1 row returned\n")
+			} else {
+				fmt.Printf("%d rows returned\n", result.Results.RowCount)
+			}
+
 		case *service.ExecuteSQLStatementResponse_Error:
 			fmt.Printf("error: %s\n", result.Error)
 		}
 	}
+}
+
+func toColumnTypes(result *service.ResultSet) (names []string, types []common.ColumnType) {
+	types = make([]common.ColumnType, len(result.Columns))
+	names = make([]string, len(result.Columns))
+	for i, in := range result.Columns {
+		columnType := common.ColumnType{
+			Type: common.Type(in.Type),
+		}
+		if in.Type == service.ColumnType_COLUMN_TYPE_DECIMAL {
+			if params := in.DecimalParams; params != nil {
+				columnType.DecScale = int(params.DecimalScale)
+				columnType.DecPrecision = int(params.DecimalPrecision)
+			}
+		}
+		types[i] = columnType
+		names[i] = in.Name
+	}
+	return
 }
