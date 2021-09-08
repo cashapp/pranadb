78e87b67d014b345bdd5259182420996b0586d41
purplefox <timvolpe@gmail.com>
2021-09-08 14:47:12 +0100

    Re-implemented fake_kafka_test.TestIngestConsumeTwoSubscribersOneGroup

--- kafka/fake_kafka_test.go
+++ kafka/fake_kafka_test.go
@@ -2,7 +2,11 @@ package kafka
 
 import (
 	"fmt"
+	log "github.com/sirupsen/logrus"
+	"github.com/squareup/pranadb/common/commontest"
 	"github.com/stretchr/testify/require"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -99,6 +103,105 @@ func TestIngestConsumeOneSubscriber(t *testing.T) {
 	group, ok := topic.getGroup(groupID)
 	require.True(t, ok)
 	require.Equal(t, 1, len(group.subscribers))
+}
+
+func TestIngestConsumeTwoSubscribersOneGroup(t *testing.T) {
+	fk := NewFakeKafka()
+	parts := 1000
+	topic, err := fk.CreateTopic("topic1", parts)
+	require.NoError(t, err)
+	var numMessages int64 = 10
+	groupID := "group1"
+	var msgCounter int64
+	consumer1 := newConsumer(groupID, topic, &msgCounter, numMessages)
+	consumer2 := newConsumer(groupID, topic, &msgCounter, numMessages)
+	consumer1.start()
+	consumer2.start()
+
+	sentMsgs := sendMessages(t, fk, int(numMessages), topic.Name)
+
+	commontest.WaitUntil(t, func() (bool, error) {
+		return atomic.LoadInt64(&msgCounter) == numMessages, nil
+	})
+
+	recvMsgs := make(map[string][]byte)
+	for _, msg := range consumer1.getMessages() {
+		recvMsgs[string(msg.Key)] = msg.Value
+	}
+	for _, msg := range consumer2.getMessages() {
+		recvMsgs[string(msg.Key)] = msg.Value
+	}
+
+	for _, sentMsg := range sentMsgs {
+		_, ok := recvMsgs[string(sentMsg.Key)]
+		require.True(t, ok, fmt.Sprintf("did not receive msg %s", string(sentMsg.Key)))
+	}
+}
+
+func newConsumer(groupID string, topic *Topic, msgCounter *int64, maxMessages int64) *consumer {
+	return &consumer{
+		groupID:     groupID,
+		topic:       topic,
+		msgCounter:  msgCounter,
+		maxMessages: maxMessages,
+	}
+}
+
+type consumer struct {
+	lock        sync.Mutex
+	groupID     string
+	topic       *Topic
+	msgCounter  *int64
+	msgs        []*Message
+	maxMessages int64
+	err         error
+}
+
+func (c *consumer) start() {
+	go func() {
+		err := c.runLoop()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+}
+
+func (c *consumer) runLoop() error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	subscriber, err := c.topic.CreateSubscriber(c.groupID, c.rebalance)
+	if err != nil {
+		return err
+	}
+	for {
+		msg, err := subscriber.GetMessage(10 * time.Millisecond)
+		if err != nil {
+			return err
+		}
+		if msg != nil {
+			c.msgs = append(c.msgs, msg)
+			offsets := make(map[int32]int64)
+			offsets[msg.PartInfo.PartitionID] = msg.PartInfo.Offset + 1
+			if err := subscriber.commitOffsets(offsets); err != nil {
+				return err
+			}
+			atomic.AddInt64(c.msgCounter, 1)
+		}
+		if atomic.LoadInt64(c.msgCounter) == c.maxMessages {
+			return nil
+		}
+	}
+}
+
+func (c *consumer) rebalance() error {
+	log.Println("rebalance occurred")
+	return nil
+}
+
+func (c *consumer) getMessages() []*Message {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.msgs
 }
 
 func sendMessages(t *testing.T, fk *FakeKafka, numMessages int, topicName string) []*Message {
