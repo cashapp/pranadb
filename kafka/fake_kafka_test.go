6d264315db1a8d045ce0fc01d597c103cd94b61f
purplefox <timvolpe@gmail.com>
2021-09-09 08:15:06 +0100

    lint

--- kafka/fake_kafka_test.go
+++ kafka/fake_kafka_test.go
@@ -187,10 +187,7 @@ func (c *consumer) runLoop() error {
 			atomic.AddInt64(c.msgCounter, 1)
 		}
 		if atomic.LoadInt64(c.msgCounter) == c.maxMessages {
-			if err := subscriber.Unsubscribe(); err != nil {
-				return err
-			}
-			return nil
+			return subscriber.Unsubscribe()
 		}
 	}
 }
