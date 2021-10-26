e786542fe249fba016113cd768c08982e2f25f95
purplefox <timvolpe@gmail.com>
2021-10-26 16:19:16 +0100

    Upgrade to Kafka 2.8.1

--- scripts/start_kafka.sh
+++ scripts/start_kafka.sh
@@ -6,13 +6,13 @@ rm -rf /tmp/kafka-logs
 rm -rf /tmp/zookeeper/*
 
 kafkadir="kafkaserver"
-kafka_version="2.8.0"
+kafka_version="2.8.1"
 kafka_archive=${kafkadir}/kafka-"${kafka-version}".tgz
 mkdir -p ${kafkadir}
 
 if [ ! -f "${kafka_archive}" ]; then
   echo "downloading and installing kafka"
-  curl https://www.mirrorservice.org/sites/ftp.apache.org/kafka/2.8.0/kafka_2.13-${kafka_version}.tgz --output "${kafka_archive}"
+  curl https://www.mirrorservice.org/sites/ftp.apache.org/kafka/${kafka_version}/kafka_2.13-${kafka_version}.tgz --output "${kafka_archive}"
   tar -xf ${kafka_archive} -C ${kafkadir}
 fi
 
