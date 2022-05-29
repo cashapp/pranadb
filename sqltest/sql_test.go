55acccfa3af6cf5c45abde785107ea56310e26bf
purplefox <timvolpe@gmail.com>
2022-05-29 20:12:03 +0100

    try disabling heartbeat timeout

--- sqltest/sql_test.go
+++ sqltest/sql_test.go
@@ -211,7 +211,7 @@ func (w *sqlTestsuite) setupPranaCluster() {
 			cnf.EnableFailureInjector = true
 			cnf.ScreenDragonLogSpam = true
 			cnf.DisableShardPlacementSanityCheck = true
-			cnf.NotifierHeartbeatInterval = 30 * time.Second
+			cnf.NotifierHeartbeatInterval = 100 * time.Hour
 
 			// We set snapshot settings to low values so we can trigger more snapshots and exercise the
 			// snapshotting - in real life these would be much higher
