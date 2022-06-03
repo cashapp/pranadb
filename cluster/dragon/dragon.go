267f3fc4b13aec5330b2c0dc0e50e82b82ff6977
purplefox <timvolpe@gmail.com>
2022-06-03 11:04:13 +0100

    increased electionrtt again

--- cluster/dragon/dragon.go
+++ cluster/dragon/dragon.go
@@ -663,7 +663,7 @@ func (d *Dragon) joinShardGroup(shardID uint64, nodeIDs []int, ch chan error) {
 
 	rc := config.Config{
 		NodeID:             uint64(d.cnf.NodeID + 1),
-		ElectionRTT:        50,
+		ElectionRTT:        100,
 		HeartbeatRTT:       2,
 		CheckQuorum:        true,
 		SnapshotEntries:    uint64(d.cnf.DataSnapshotEntries),
