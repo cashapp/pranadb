c28f714f147fa4eff60266813de79240833e1dc9
Tim Fox <purplefox@users.noreply.github.com>
2022-07-18 20:30:05 +0100

    Make sure init is run to set dragon logadaptor (#484)
    * Make sure init is run to set dragon logadaptor and fixed race

--- cluster/dragon/logadaptor/logrus_adaptor.go
+++ cluster/dragon/logadaptor/logrus_adaptor.go
@@ -3,48 +3,49 @@ package logadaptor
 import (
 	"github.com/lni/dragonboat/v3/logger"
 	log "github.com/sirupsen/logrus"
+	"sync/atomic"
 )
 
 /*
 This adaptor allows us to plug the dragonboat logging into the logrus logger we use in Prana.
 */
 
-func init() {
-	logger.SetLoggerFactory(logrusLogFactory)
-}
-
-func logrusLogFactory(pkgName string) logger.ILogger {
+func LogrusLogFactory(pkgName string) logger.ILogger {
 	return &LogrusILogger{}
 }
 
 type LogrusILogger struct {
-	level logger.LogLevel
+	level int64
+}
+
+func (l *LogrusILogger) getLevel() logger.LogLevel {
+	return logger.LogLevel(atomic.LoadInt64(&l.level))
 }
 
 func (l *LogrusILogger) SetLevel(level logger.LogLevel) {
-	l.level = level
+	atomic.StoreInt64(&l.level, int64(level))
 }
 
 func (l *LogrusILogger) Debugf(format string, args ...interface{}) {
-	if l.level >= logger.DEBUG {
+	if l.getLevel() >= logger.DEBUG {
 		log.Debugf(format, args...)
 	}
 }
 
 func (l *LogrusILogger) Infof(format string, args ...interface{}) {
-	if l.level >= logger.INFO {
+	if l.getLevel() >= logger.INFO {
 		log.Infof(format, args...)
 	}
 }
 
 func (l *LogrusILogger) Warningf(format string, args ...interface{}) {
-	if l.level >= logger.WARNING {
+	if l.getLevel() >= logger.WARNING {
 		log.Warnf(format, args...)
 	}
 }
 
 func (l *LogrusILogger) Errorf(format string, args ...interface{}) {
-	if l.level >= logger.ERROR {
+	if l.getLevel() >= logger.ERROR {
 		log.Errorf(format, args...)
 	}
 }
